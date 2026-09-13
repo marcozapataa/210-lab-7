efd4915d016a966abc7cb9600f8bb8ef3807ee09
Marco <mazapata053@gmail.com>
2026-09-13 16:34:58 -0700

    added function prototype and definition for reverseArray function

--- main.cpp
+++ main.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
+
+string* reverseArray(string* arr, int size);
 
 int main()
 {
 
     return 0;
+}
+
+//Function definiton for reverseArray
+string* reverseArray(string* arr, int size)
+{
+    string* start = arr;
+    string* end = arr + size - 1;
+
+    while (start < end)
+    {
+        swap(*start, *end)
+        start++;
+        end--;
+    }
+    return arr;
 }
